06078e2a7b8e9fc8147f42342ea896e023d36958
Carlos Morales <developer.caros.morales@gmail.com>
2024-02-09 04:20:18 -0600

    Pull to refresh

--- lib/src/shared/routes/routes.dart
+++ lib/src/shared/routes/routes.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:fulltime_force_task/src/pages/home/ui/home.dart';
 import 'package:fulltime_force_task/src/sample_feature/sample_item_list_view.dart';
 import 'package:fulltime_force_task/src/pages/settings/settings_view.dart';
 import 'package:go_router/go_router.dart';
 
 class Routes {
   static const String home = '/home';
-  static const String branchs = '/branchs';
+  static const String branchs = '/branches';
   static const String commits = '/settings';
   static const String settings = '/settings';
 }
@@ -20,12 +21,17 @@ var routes = GoRouter(
     GoRoute(
       name: Routes.home,
       path: '/home',
-      builder: (context, state) => const SampleItemListView(),
+      builder: (context, state) => const Home(),
     ),
     GoRoute(
       name: Routes.settings,
       path: '/settings',
       builder: (context, state) => const SettingsView(),
+    ),
+    GoRoute(
+      name: Routes.branchs,
+      path: '/branches',
+      builder: (context, state) => const SampleItemListView(),
     ),
   ],
 );
